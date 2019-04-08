351de8bde22f92dce0d3d65b0965a3a16941112d
tranphuquy19 <tranphuquy19@gmail.com>
2019-04-08 18:58:45 +0700

    edit User model

--- db/migrate/20190404091834_create_users.rb
+++ db/migrate/20190404091834_create_users.rb
@@ -7,6 +7,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :confirmation_token, limit: 128
       t.string :remember_token, limit: 128, null: false
       t.integer :role, null: false, :default => 1 #normal User, 2 admin
+      t.string :name, limit: 128
     end
 
     add_index :users, :email
