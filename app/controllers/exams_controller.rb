4377493abee64c08c2c45a199eb75c22db26d221
tranChiVi0123 <idiots1230@gmail.com>
2019-04-09 21:24:30 +0700

    views_exam

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -1,2 +1,12 @@
 class ExamsController < ApplicationController
+    def show
+        render html:"exam HERE!"
+    end
+    
+    def update
+    end
+
+    def deytroy
+    end
+    
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
   get '/posts/:id/md', to: 'posts#markdown'
   get '/home', to: 'home#show'
+  get '/exams', to: 'exams#show'
+
   resources :posts
   resources :tests
   resources :test_sessions
+  resources :exams
   root 'home#show', :as => :home_page
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
