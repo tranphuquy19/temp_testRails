a37be8be3e7078ea5db7910a3595759a4b75d859
tranphuquy19 <tranphuquy19@gmail.com>
2019-04-09 17:38:39 +0700

    views test_sessions

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,6 @@ class HomeController < ApplicationController
     def show
         @title = "Home"
         @posts = Post.last(8).reverse
-        @tests = TestSession.last(12).reverse
+        @test_sessions = TestSession.last(12).reverse
     end
 end
