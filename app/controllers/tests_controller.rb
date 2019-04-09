f20121baca4f85ce9a3888ebc5402284306ea159
tranChiVi0123 <idiots1230@gmail.com>
2019-04-09 07:39:37 +0700

    deleted test

---
+++ app/controllers/tests_controller.rb
@@ -0,0 +1,6 @@
+class TestsController < ApplicationController
+    
+    def quiz
+    end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
 
   root 'home#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get 'quiz'=>'tests#quiz'
+  resources :test
+  
 end
