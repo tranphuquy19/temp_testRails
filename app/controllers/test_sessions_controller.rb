0bba9ce729709f16d483e9d340064d645e30207c
tranphuquy19 <tranphuquy19@gmail.com>
2019-04-19 15:57:41 +0700

    fix multi TestPaper

--- app/controllers/test_sessions_controller.rb
+++ app/controllers/test_sessions_controller.rb
@@ -83,7 +83,9 @@ class TestSessionsController < ApplicationController
         pars = params[:session]
         @test_session = TestSession.find(pars[:submit])
         if allow_examinations
-            TestPaper.create(exam_id: pars[:submit].to_i, test_session_id: @test_session.id, category_id: @test_session.category.id, user_id: current_user.id)
+            if(TestPaper.where(test_session_id: @test_session.id, user_id: current_user.id).emty? == true)
+                TestPaper.create(exam_id: pars[:submit].to_i, test_session_id: @test_session.id, category_id: @test_session.category.id, user_id: current_user.id)
+            end
             redirect_to exams_show_path
         else
             redirect_to home_path
