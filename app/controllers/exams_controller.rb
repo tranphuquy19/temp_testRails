14a0fd54f89b3eb1bbd785b5b1194fb6e556750f
tranphuquy19 <tranphuquy19@gmail.com>
2019-04-20 01:07:21 +0700

    update

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -3,21 +3,32 @@ class ExamsController < ApplicationController
     include ApplicationHelper
     before_action :ifNotAdmin
 
+    def index
+        @title = "Exams"
+        @exams = Exam.last(30).reverse
+    end
+
+    def new
+    end
+
     def show
     end
 
     def create   
+        pars = params[:exam]
+        list_questions = pars[:list_questions].split("\r\n").map{|tag| tag.strip}.join(",")
+        Exam.create(title: pars[:title], list_questions: list_questions, category_id: Category.find_by(title: pars[:category]))
     end
     
     def update
         pars = params[:exam]
         list_questions = pars[:list_questions].split("\r\n").map{|tag| tag.strip}.join(",")
-        p list_questions
-        Exam.find(pars[:id].to_i).update_attributes(title: pars[:title], category_id: Category.find_by(title: pars[:category]))
+        Exam.find(pars[:id].to_i).update_attributes(title: pars[:title], category_id: Category.find_by(title: pars[:category]).id)
     end
     
 
     def edit
+        @title = "Edit exam"
         @exam = Exam.find(params[:id])
     end
 
